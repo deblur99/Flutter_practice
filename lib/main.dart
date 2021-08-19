ae5095c44241926ee18095f2dc88bda81ed7a4f0
deblur99 <deblur99@kakao.com>
2021-08-19 15:56:08 +0900

    switch_color_by_clicking_button

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,98 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyApp extends State<MyApp> {
+  bool stateValue = false;
+  Color buttonColor = Colors.greenAccent;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData.light(),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Text(
+                  "Hello flutter",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blueAccent, fontSize: 20),
+                ),
+                Switch(
+                  value: stateValue,
+                  onChanged: (value) {
+                    setState(() {
+                      print(value);
+                      stateValue = value;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("Hello button!"),
+                  // swap background color of button between greenAccent and deepPurpleAccent
+                  style: ElevatedButton.styleFrom(
+                    primary: buttonColor, // change the color of button by clicking the button
+
+                  ),
+                  onPressed: () {
+                    print(buttonColor);
+                    if (buttonColor == Colors.greenAccent) {
+                      setState(() {
+                        buttonColor = Colors.deepPurpleAccent;
+                      });
+                    } else {
+                      setState(() {
+                        buttonColor = Colors.greenAccent;
+                      });
+                    }
+                  },
+                  onLongPress: () => showDialog<String> (
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      title: const Text("AlertDialog Title"),
+                      content: const Text("AlertDialog description"),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, "Cancel"),
+                          child: const Text("CANCEL"),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, "OK"),
+                          child: const Text("OK"),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<MyApp> {
   // This widget is the root of your application.
   var switchValue = false;
-  String text = "Hello";
+  String test = "hello";
+  Color _color = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,16 +125,26 @@ class _MyApp extends State<MyApp> {
         home: Scaffold(
             // 배경색은 Container에서, 본문색은 Text 내 TextStyle의 color 속성에서 정의한다.
             body: Center(
-              child: Switch(
-                value: switchValue,
-                onChanged: (value) {
-                  setState(() {
-                    print(value);
-                    switchValue = value;
-                  });
-                }),
+              child: RaisedButton(
+                child: Text("$test"),
+                color: _color,
+                onPressed: () {
+                  if (_color == Colors.blue) {
+                    setState(() {
+                      test = "flutter";
+                      _color = Colors.amber;
+                    });
+                  } else {
+                    setState(() {
+                      test = "hello";
+                      _color = Colors.blue;
+                    });
+                  }
+                },
+              ),
             )
         )
     );
   }
 }
+*/
